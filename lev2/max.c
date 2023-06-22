4a2c13766537ed7c4c5bc6e933c0a17f0638282c
majrou <36647431+ajrou4@users.noreply.github.com>
2023-06-22 21:13:07 +0100

    Update max.c

--- lev2/max.c
+++ lev2/max.c
@@ -1,7 +1,7 @@
 int		max(int* tab, unsigned int len)
 {
     int i = 0;
-<<<<<<< HEAD
+
     int max = tab[i];
 
     while(i < len)
@@ -11,15 +11,4 @@ int		max(int* tab, unsigned int len)
         i++;
     }
     return max;
-=======
-    int max ;
-     max = tab[i];
-     while (i < len)
-     {
-		if(max < tab[i])
-			max = tab[i];
-		i++;
-     }
-     return max;
->>>>>>> 0de1286ad6ddc929f999250ddc6322734aff82fb
 }
