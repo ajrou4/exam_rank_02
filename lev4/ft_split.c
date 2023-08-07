9ce2a38e4511d7694916c3f52cd276943e40a0af
majrou <36647431+ajrou4@users.noreply.github.com>
2023-08-07 15:10:30 +0100

    Update ft_split.c

--- lev4/ft_split.c
+++ lev4/ft_split.c
@@ -1,15 +1,5 @@
 #include <stdlib.h>
 
-// char *ft_strncpy(char *s1, char *s2, int n)
-// {
-// 	int i = -1;
-
-// 	while (++i < n && s2[i])
-// 		s1[i] = s2[i];
-// 	s1[i] = '\0';
-// 	return (s1);
-// }
-
 char *ft_strncpy(char *s1, char *s2, int n)
 {
     int i =-1;
@@ -18,50 +8,6 @@ char *ft_strncpy(char *s1, char *s2, int n)
     s1[i] = '\0';
     return 0;
 }
-
-// char **ft_split(char *str) 
-// {
-//     char **words = NULL;
-//     int wc = 0;
-//     int i = 0, j = 0, k;
-//     while (str[i] != '\0') 
-//     {
-//         if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n') {
-//             wc++;
-//             while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
-//                 i++;
-//         }
-//         else
-//             i++;
-//     }
-//     words = (char **)malloc((wc + 1) * sizeof(char *));
-//     if (words == NULL) 
-//         return NULL;
-//     i = 0;
-//     j = 0;
-//     while (str[i] != '\0') 
-//     {
-//         if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n') 
-//         {
-//             k = 0;
-//             while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0') 
-//             {
-//                 i++;
-//                 k++;
-//             }
-//             words[j] = (char *)malloc((k + 1) * sizeof(char));
-//             if (words[j] == NULL) 
-//                 return NULL;
-//             ft_strncpy(words[j], &str[i - k], k);
-//             words[j][k] = '\0';
-//             j++;
-//         } 
-//         else 
-//             i++;
-//     }
-//     words[j] = NULL; 
-//     return words;
-// }
 
 char **ft_split(char *str)
 {
